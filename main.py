aa5d855aea37efe8da284be90982bd68786a2416
PedroAntunes <35009178+pedronmantunes@users.noreply.github.com>
2019-08-31 23:10:34 +0100

    Update main.py

--- main.py
+++ main.py
@@ -3,10 +3,9 @@ from tkinter import *
 from amov import *
 #from mpl_toolkits.mplot3d import axes3d
 #import time
-
-def pass_doc():
+def pass_doc(data):
     doc = doc_name.get()
-    get_file(doc)
+    data = get_file(doc)
 
 root = Tk()
 # Code to add widgets will go here...
@@ -14,7 +13,7 @@ L1 = Label(root, text="Doc Name: ")
 L1.pack(side = LEFT)
 doc_name = Entry(root, bd =5)
 doc_name.pack(side = LEFT)
-B1 = Button(root, text='Get File', command=pass_doc)
+B1 = Button(root, text='Get File', command=lambda: pass_doc(data))
 B1.pack(side = LEFT)
 
 root.mainloop()
