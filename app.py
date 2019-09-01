35f4424d510657c86f1f791d9338f5c62960df8b
PedroAntunes <35009178+pedronmantunes@users.noreply.github.com>
2019-09-01 04:25:48 +0100

    Update app.py

--- app.py
+++ app.py
@@ -19,6 +19,15 @@ style.use('ggplot')
 f = plt.figure(2)
 a = f.add_subplot(111)
 
+def popupmsg(msg):
+    popup = tk.Tk()
+    popup.wm_title("!")
+    label = ttk.Label(popup, text=msg, font=NORM_FONT)
+    label.pack(side="top", fill="x", pady=10)
+    B1 = ttk.Button(popup, text="Okay", command = popup.destroy)
+    B1.pack()
+    popup.mainloop()
+
 
 class AMov(tk.Tk):
 
